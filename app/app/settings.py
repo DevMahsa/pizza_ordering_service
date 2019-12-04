c56e2551cbbb377538ba15c6fe0bb6f23dc4fa0d
=Mahsa <mahsa.gol89@gmail.com>
2019-12-04 14:33:05 +0330

    allowed_host in settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'oc-2u0)*xw4--7qi4fkd4#bm+t*f0!wq8%m9!z9lvj&da_#w@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
